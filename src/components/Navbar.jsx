5330057b710a00b256fa9b32e8db5ebf46a2d65e
khalid-dev <ka1239996@gmail.com>
2025-02-21 17:56:18 +0530

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 import { styles } from "../styles";
 import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
@@ -41,6 +42,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
+          <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             Khalid &nbsp;
             <span className='sm:block hidden'> | Ui Designer</span>
